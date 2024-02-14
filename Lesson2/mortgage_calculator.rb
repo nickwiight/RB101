3726a5750f508bbfbaf73839beb7b33439f3f247
Nick White <nickwiight@gmail.com>
2024-02-13 19:45:31 -0800

    Mortgage calculator refactor

--- Lesson2/mortgage_calculator.rb
+++ Lesson2/mortgage_calculator.rb
@@ -1,49 +1,130 @@
-def monthly_payment(loan, apr, duration)
-  mpr = (apr / (100.0 * 12))
-  months = duration * 12
-  monthly_payment = (loan * (mpr / (1 - (1 + mpr)**-months))).round(2)
+require 'yaml'
 
-  prompt "Monthly interest rate: #{(mpr * 100).round(3)}"
-  prompt "Loan duration in months: #{months.to_i}"
-  prompt "Monthly payment: $#{monthly_payment}"
+MESSAGES = YAML.load_file 'mortgage_calculator_messages.yml'
+
+def get_loan_info_from_user
+  display_personalized_or_generic_message('loan_amount', NAME)
+  loan = number_input
+  display_personalized_or_generic_message('apr_amount', NAME)
+  apr = number_input
+  display_personalized_or_generic_message('loan_duration', NAME)
+  duration_in_years = number_input
+
+  return loan, apr, duration_in_years
 end
 
-def prompt(message)
-  puts ">> #{message}"
+def get_name_input
+  display_message 'enter_name'
+  name = ''
+  loop do
+    name = gets.chomp
+    if name == ''
+      display_message 'generalized'
+
+    else
+      display_message('your_name', name)
+    end
+    break if confirmation?
+
+    display_message 'enter_name'
+  end
+  name
+end
+
+def calculate_monthly_payment(loan, apr, duration_in_years)
+  mpr = (apr / (100.0 * 12))
+  duration_in_months = duration_in_years * 12
+  monthly_payment = (loan *
+    (mpr / (1 - ((1 + mpr)**(-duration_in_months))))).round(2)
+
+  return mpr, monthly_payment
+end
+
+def display_monthly_payment(mpr, monthly_payment)
+  clear_screen
+  display_personalized_or_generic_message('monthly_breakdown', NAME)
+  sleep 1
+  display_message('monthly_interest', (mpr * 100).round(3))
+  sleep 1
+  display_message('monthly_payment', monthly_payment)
+  sleep 1
 end
 
 def number?(string)
   string.to_i.to_s == string || string.to_f.to_s == string
 end
 
-def not_negative(string)
+def not_negative?(string)
   string.to_f.positive?
 end
 
-def valid_input(message)
+def number_input
   input = ''
-  prompt message
   loop do
     input = gets.chomp
-    break if number?(input) && not_negative(input)
+    break if valid_number_input?(input)
 
-    prompt 'You must enter a positive number. Try again.'
+    display_message 'positive_number'
   end
   input.to_f
 end
 
-prompt 'Welcome to Mortgage Calculator'
-
-loop do
-  loan = valid_input('What is your loan amount?')
-  apr = valid_input('What is your APR? (ex: 5.3 for 5.3%)')
-  duration = valid_input('What is your loan duration in years?')
-  monthly_payment(loan, apr, duration)
-
-  prompt 'Would you like to calculate another mortgage? (Y to continue)'
-  continue = gets.chomp
-  break unless continue.downcase.start_with?('y')
+def valid_number_input?(input)
+  number?(input) && not_negative?(input)
 end
 
-prompt 'Thank you for using Mortgage Calculator.'
-prompt 'Goodbye!'
+def clear_screen
+  system 'clear'
+end
+
+def confirmation?
+  input = ''
+  loop do
+    input = gets.chomp
+    break if valid_confirmation_input? input
+
+    display_message 'invalid_confirmation'
+  end
+
+  input.start_with? 'y'
+end
+
+def valid_confirmation_input?(string)
+  (string.start_with? 'y') || (string.start_with? 'n')
+end
+
+def display_personalized_or_generic_message(key, *args)
+  key += '_generic' if NAME.empty?
+  display_message(key, *args)
+end
+
+def display_message(key, *args)
+  prompt(message(key, *args))
+end
+
+def message(key, *args)
+  args.empty? ? MESSAGES[key] : MESSAGES[key] % args
+end
+
+def prompt(message)
+  puts ">> #{message}"
+end
+
+clear_screen
+display_message 'welcome'
+NAME = get_name_input
+display_personalized_or_generic_message('thanks_name', NAME)
+
+loop do
+  clear_screen
+  loan, apr, years = get_loan_info_from_user
+  mpr, monthly_payment = calculate_monthly_payment(loan, apr, years)
+  display_monthly_payment(mpr, monthly_payment)
+
+  display_personalized_or_generic_message('calculate_another', NAME)
+  break unless confirmation?
+end
+
+clear_screen
+display_message 'thanks'
+display_personalized_or_generic_message('goodbye', NAME)
